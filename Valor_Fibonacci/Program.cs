bc8abe9137ab91dc1c383ad28947a336b66b8b23
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-23 15:58:00 -0300

    Adicionar arquivos de projeto.

---
+++ Valor_Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Valor_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, i, nAnt = 0, nPost = 1, x;
+            
+            Console.WriteLine("Digite um valor!");
+
+            valor = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nEssa e a sequencia de Fibonacci!");
+            Console.WriteLine(x = 0);
+            Console.WriteLine(nPost);
+            
+
+            for (i = 0; i <= valor; i++)
+            {
+                x = nAnt;
+                nAnt = nPost;
+                nPost = x + nAnt;
+                Console.WriteLine(nPost);               
+
+            }
+            Console.WriteLine("\n\nClique enter para SAIR");
+        }
+    }
+}
+
+
+
+
+
+
+//.Escreva um programa que leia um valor e imprima a sequência de Fibonacci 
+//usando o valor digitado como posição de parada
